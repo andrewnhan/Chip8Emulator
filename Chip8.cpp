aabf1ed984903613dfadcf7f66af47fd1f90bdc5
Andrew Nguyen <andrewnhan1@gmail.com>
2020-09-21 22:37:35 -0400

    function pointer map for ops, add in a few op functions, change stack to be C++ stack

--- Chip8.cpp
+++ Chip8.cpp
@@ -4,11 +4,9 @@ Chip8::Chip8() {
   programCounter = START_ADDRESS;
   opCode = 0;
   indexRegister = Register(0);
-  stackPointer = 0;
 
   for (int i = 0; i < 16; i++) {
     gprs.push_back(Register(0));
-    stack.push_back(0);
     keys.push_back(0);
   }
   for (int i = 0; i < 4096; i++) {
@@ -17,6 +15,10 @@ Chip8::Chip8() {
   for (int i = 0; i < 2048; i++) {
     graphicsBuffer.push_back(i);
   }
+
+  zeroOps.insert(std::make_pair(0x00, &Chip8::Op0nnn));
+  zeroOps.insert(std::make_pair(0xee, &Chip8::Op00e0));
+  zeroOps.insert(std::make_pair(0xe0, &Chip8::Op00ee));
 
   delayTimer = 0;
   soundTimer = 0;
@@ -49,7 +51,49 @@ void Chip8::cycle() {
 
 	programCounter += 2;
 
-	std::cout << std::hex << static_cast<int>(instruction) << std::endl;
 
+}
 
+// Ops
+void Chip8::Op0nnn(uint16_t instruction) {
+	programCounter = instruction & (0x0FFFu);
+}
+
+void Chip8::Op00e0(uint16_t instruction) {
+	// clear the display
+	// empty
+}
+
+void Chip8::Op00ee(uint16_t instruction) {
+	programCounter = functionStack.top();
+	functionStack.pop();
+}
+
+void Chip8::Op1nnn(uint16_t instruction) {
+	programCounter = instruction & (0x0FFFu);
+}
+
+void Chip8::Op2nnn(uint16_t instruction) {
+	if(functionStack.size() > 16) {
+		// Throw an exception here in the future.
+		return;
+	}
+	functionStack.push(programCounter);
+	programCounter = instruction & (0x0FFFu);
+}
+
+void Chip8::Op3xkk(uint16_t instruction) {
+	uint8_t registerNumber = instruction & (0x0F00u);
+	uint8_t comparisonValue = instruction & (0x00FFu);
+	if(gprs.at(registerNumber).value == comparisonValue) {
+		programCounter += 2;
+	}
+}
+
+void Chip8::Op4xkk(uint16_t instruction) {
+	uint8_t registerNumber = instruction & (0x0F00u);
+	uint8_t comparisonValue = instruction & (0x00FFu);
+	if(gprs.at(registerNumber).value != comparisonValue) {
+		programCounter += 2;
+	}
 }
--- Chip8.h
+++ Chip8.h
@@ -1,41 +1,57 @@
 #include <cstdint>
 #include <vector>
+#include <map>
+#include <stack>
 #include <iostream>
 #include <fstream>
 
 const int START_ADDRESS = 0x200;
 
+
 class Chip8 {
 private:
-  class Register {
-  public:
-    Register(uint8_t val = 0) : value(val){};
-    uint8_t value;
-  };
+	typedef void (Chip8::*Chip8Op)(uint16_t); 
 
-  // General purpose register, 16
-  std::vector<Register>
+	class Register {
+	public:
+		Register(uint8_t val = 0) : value(val){};
+		uint8_t value;
+	};
+
+	void Op0nnn(uint16_t instruction);
+	void Op00e0(uint16_t instruction);
+	void Op00ee(uint16_t instruction);
+	void Op1nnn(uint16_t instruction);
+	void Op2nnn(uint16_t instruction);
+	void Op3xkk(uint16_t instruction);
+	void Op4xkk(uint16_t instruction);
+	
+	
+	
+	std::map<uint8_t, Chip8Op> zeroOps;
+
+  	// General purpose register, 16
+	std::vector<Register>
       gprs;
-  // Memory
-  std::vector<uint8_t> memory;
-  // Index register
-  Register indexRegister;
-  // PC
-  uint16_t programCounter;
-  uint16_t opCode;
-  // Stack information, 16 stack levels
-  std::vector<uint16_t> stack;
-  uint8_t stackPointer;
-  // Timer
-  uint8_t delayTimer;
-  uint8_t soundTimer;
-  // keys
-  std::vector<uint8_t> keys;
-  // graphics
-  std::vector<uint32_t> graphicsBuffer;
+  	// Memory
+  	std::vector<uint8_t> memory;
+  	// Index register
+  	Register indexRegister;
+  	// PC
+  	uint16_t programCounter;
+  	uint16_t opCode;
+  	// Stack information, 16 stack levels
+  	std::stack<uint16_t> functionStack;
+  	// Timer
+  	uint8_t delayTimer;
+  	uint8_t soundTimer;
+  	// keys
+  	std::vector<uint8_t> keys;
+  	// graphics
+  	std::vector<uint32_t> graphicsBuffer;
 
 public:
-  Chip8();
-  bool loadRom(const char* fileNames);
-  void cycle();
+	Chip8();
+	bool loadRom(const char* fileNames);
+	void cycle();
 };
